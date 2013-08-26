9b5ae43edf2d63479cddd46770195a6bbe87089d
lars-christian.simon <lars-christian.simon@boiii.de>
2013-08-26 14:09:45 +0200

    Added ReflectionUtils. Prepared UI for testing.

--- playground/src/main/java/de/lsn/client/UI.java
+++ playground/src/main/java/de/lsn/client/UI.java
@@ -1,42 +1,68 @@
 package de.lsn.client;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang.math.NumberUtils;
+
+import de.lsn.resource.Constants;
 import de.lsn.resource.UIPhase;
 
 public class UI {
 
 	private static boolean runnable = true;
+	
+	private static String url = Constants.firebaseURL;
 
-	private static String mainMenu = "[1] READ\n" + "[2] WRITE\n" + "[0] EXIT"
-			+ ": ";
-	private static String readMenu = "[3] MAIN" + ": ";
-	private static String writeMenu = "[3] MAIN" + ": ";
+	private static String mainMenu = "[1] READ\n" + "[2] WRITE\n" + "[EXIT] EXIT" + ": ";
+	private static String readMenu = "[1] SEND GET TO " + url + "\n" + "[RET] RETURN" + ": ";
+	private static String writeMenu = "[1] SEND PUT TO " + url + "\n" + "[RET] RETURN" + ": ";
 
 	private static Scanner scan = new Scanner(System.in);
 
-	private static UIPhase lastPhase = UIPhase.MAIN_MENU;
+	private static UIPhase parentPhase = UIPhase.MAIN_MENU;
 
+	private static void startUp() {
+		showUI(UIPhase.MAIN_MENU);
+	}
+	
 	private static void showUI(UIPhase phase) {
 		UIPhase newPhase = phase;
 		while (runnable)
 			switch (phase) {
 			case MAIN_MENU:
 				newPhase = UIPhase.stringToPhase(promptMainMenu());
-				lastPhase = phase;
+				parentPhase = phase;
 				showUI(newPhase);
 				break;
 			case READ_MENU:
 				System.out.println("READ MENU");
+				parentPhase = UIPhase.MAIN_MENU;
 				newPhase = UIPhase.stringToPhase(promptReadMenu());
-				lastPhase = phase;
+				showUI(newPhase);
+				break;
+			case SEND_GET:
+				System.out.println("SEND_GET not yet implemented!");
+				parentPhase = UIPhase.READ_MENU;
+				newPhase = parentPhase;
+				newPhase = parentPhase;
 				showUI(newPhase);
 				break;
 			case WRITE_MENU:
 				System.out.println("WRITE MENU");
+				parentPhase = UIPhase.MAIN_MENU;
 				newPhase = UIPhase.stringToPhase(promptWriteMenu());
-				lastPhase = phase;
 				showUI(newPhase);
+				break;
+			case SEND_PUT:
+				System.out.println("SEND_PUT not yet implemented!");
+				parentPhase = UIPhase.WRITE_MENU;
+				newPhase = parentPhase;
+				showUI(newPhase);
+				break;
+			case RETURN:
+				showUI(parentPhase);
 				break;
 			case EXIT:
 				runnable = false;
@@ -44,86 +70,68 @@ public class UI {
 				break;
 			default:
 				System.err.println("UNKNOW COMMAND!");
-				showUI(lastPhase);
+				showUI(parentPhase);
 				break;
 			}
 	}
 
-	private static int promptMainMenu() {
+	private static String promptMainMenu() {
 		System.out.println(mainMenu);
-		int readVal = scan.nextInt();
-		int res = -1;
-		if (isValid(readVal, UIPhase.MAIN_MENU)) {
+		String readVal = scan.next();
+		String res = "-1";
+		if (isValid(readVal)) {
 			res = readVal;
 		}
 		return res;
 	}
 
-	private static int promptReadMenu() {
+	private static String promptReadMenu() {
 		System.out.println(readMenu);
-		int readVal = scan.nextInt();
-		int res = -1;
-		if (isValid(readVal, UIPhase.READ_MENU)) {
+		String readVal = scan.next();
+		String res = "-1";
+		if (isValid(readVal)) {
 			res = readVal;
+			if (NumberUtils.isNumber(readVal)) {
+				res = "1"+readVal;
+			}
 		}
 		return res;
 	}
 
-	private static int promptWriteMenu() {
+	private static String promptWriteMenu() {
 		System.out.println(writeMenu);
-		int readVal = scan.nextInt();
-		int res = -1;
-		if (isValid(readVal, UIPhase.WRITE_MENU)) {
+		String readVal = scan.next();
+		String res = "-1";
+		if (isValid(readVal)) {
 			res = readVal;
+			if (NumberUtils.isNumber(readVal)) {
+				res = "2"+readVal;
+			}
 		}
 		return res;
 	}
 
-	private static boolean isValid(int value, UIPhase phase) {
+	private static boolean isValid(String value) {
 		boolean res = false;
-		switch (phase) {
-		case MAIN_MENU:
-			if(1 == value){
-				res = true;
-			}
-			else if (2 == value) {
-				res = true;
-			}
-			else if (0 == value) {
-				res = true;
-			}
-			break;
-		case READ_MENU:
-			if(3 == value){
-				res = true;
-			}
-//			else if (2 == value) {
-//				res = true;
-//			}
-//			else if (0 == value) {
-//				res = true;
-//			}
-			break;
-		case WRITE_MENU:
-			if(3 == value){
-				res = true;
-			}
-//			else if (2 == value) {
-//				res = true;
-//			}
-//			else if (0 == value) {
-//				res = true;
-//			}
-			break;
-		default:
-			System.err.println("A serious error occured!");
-			break;
+		Pattern p = Pattern.compile("[0-9]");
+		Matcher matcher;
+		matcher = p.matcher(String.valueOf((value)));
+		if(isReturn(value) || isExit(value) || matcher.matches()){
+			res = true;
 		}
 		return res;
 	}
 
+	private static boolean isReturn(String value) {
+		return UIPhase.RETURN.stringValue().startsWith(value);
+	}
+
+	private static boolean isExit(String value) {
+		return UIPhase.EXIT.stringValue().startsWith(value);
+	}
+	
 	public static void main(String[] args) {
-		showUI(UIPhase.MAIN_MENU);
+		startUp();
 	}
 
 }
--- playground/src/main/java/de/lsn/resource/UIPhase.java
+++ playground/src/main/java/de/lsn/resource/UIPhase.java
@@ -2,26 +2,29 @@ package de.lsn.resource;
 
 public enum UIPhase {
 
-	EXIT(0),
-	READ_MENU(1),
-	WRITE_MENU(2),
-	MAIN_MENU(3),
-	UNKNOWN(-1);
+	READ_MENU("1"),
+	SEND_GET("11"),
+	WRITE_MENU("2"),
+	SEND_PUT("21"),
+	MAIN_MENU("3"),
+	RETURN("RETURN"),
+	EXIT("EXIT"),
+	UNKNOWN("-1");
 
-	int val = 0;
+	String stringValue = "";
 	
-	private UIPhase(int val) {
-		this.val = val;
+	private UIPhase(String stringValue) {
+		this.stringValue = stringValue;
 	}
 	
-	public int val() {
-		return val;
+	public String stringValue() {
+		return stringValue;
 	}
 	
-	public static UIPhase stringToPhase(int val) {
+	public static UIPhase stringToPhase(String val) {
 		UIPhase result = UIPhase.UNKNOWN;
 		for (UIPhase phase : UIPhase.values()) {
-			if(phase.val() == val) {
+			if(phase.stringValue().startsWith(val)) {
 				result = phase;
 				break;
 			}
